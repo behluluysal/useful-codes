0cc9e667d482c02dfa26ed5d645b94038d2d4eae
behlül uysal <s.b.uysal98@hotmail.com>
2021-03-02 15:26:25 +0300

    find child of object unity

---
+++ C#/Unity/find-child-of-object.cs
@@ -0,0 +1,11 @@
+    private GameObject _pandora;
+    private GameObject _pandoraChild;
+    Animator anim;
+
+    // Find works with object names, can be changed by tag
+    void Start()
+    {
+        _pandora = GameObject.Find("BoxOfPandora");
+        _pandoraChild = _pandora.transform.Find("Box001").gameObject;
+        anim = GetComponent<Animator>();
+    }
